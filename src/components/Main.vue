a39f23e4c802953feaf3f265da0282acd069b9df
Sontaya1607 <sontaya16071997@gmail.com>
2019-10-07 16:40:52 +0700

    Create Main in Component

---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main">
+      {{text + ' Sontaya Rungsaard'}} {{number + 3}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      text: 'Hello',
+      number: 0
+    }
+  }
+}
+</script>
+
+<style scoped></style>
